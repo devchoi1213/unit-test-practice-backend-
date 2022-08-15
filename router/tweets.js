c5740d75e7143d01e54dfe10a85d36fcb8b09c00
devchoi1213 <devchoi1213@gmail.com>
2022-08-15 12:32:52 +0900

    Refactoring tweet controller and router

--- router/tweets.js
+++ router/tweets.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
 import { body } from 'express-validator';
-import * as tweetController from '../controller/tweet.js';
 import { isAuth } from '../middleware/auth.js';
 import { validate } from '../middleware/validator.js';
 
@@ -15,20 +14,21 @@ const validateTweet = [
   validate,
 ];
 
-// GET /tweet
+export default function tweetsRouter(tweetController) {
+  // GET /tweet
 // GET /tweets?username=:username
-router.get('/', isAuth, tweetController.getTweets);
+  router.get('/', isAuth, tweetController.getTweets);
 
 // GET /tweets/:id
-router.get('/:id', isAuth, tweetController.getTweet);
+  router.get('/:id', isAuth, tweetController.getTweet);
 
 // POST /tweeets
-router.post('/', isAuth, validateTweet, tweetController.createTweet);
+  router.post('/', isAuth, validateTweet, tweetController.createTweet);
 
 // PUT /tweets/:id
-router.put('/:id', isAuth, validateTweet, tweetController.updateTweet);
+  router.put('/:id', isAuth, validateTweet, tweetController.updateTweet);
 
 // DELETE /tweets/:id
-router.delete('/:id', isAuth, tweetController.deleteTweet);
-
-export default router;
+  router.delete('/:id', isAuth, tweetController.deleteTweet);
+  return router;
+}
